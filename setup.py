0a496e9c2392b0a389947ef91ff1ef725af4f816
Jon Riehl <jriehl@spaceship.com>
2013-03-18 18:24:39 -0500

    Minor fixes to setup.py.

--- setup.py
+++ setup.py
@@ -6,11 +6,12 @@ setup(
     name = "pgen2",
     packages = ["pgen2"],
     version = "0.1.0",
-    description = "Pure Python implementation of pgen, the Python parser generator",
+    description = "Pure Python implementation of pgen, the Python parser "
+    "generator",
     author = "Jon Riehl",
     author_email = "jon.riehl@gmail.com",
     url = "https://github.com/mythonlang/pgen2/",
-    keywords = ["parser", "parser generator"],
+    keywords = ["parser", "generator"],
     classifiers = [
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.6",
@@ -20,7 +21,7 @@ setup(
         "Environment :: Other Environment",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
-        "Operation System :: OS Independent",
+        "Operating System :: OS Independent",
         "Topic :: Software Development",
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
